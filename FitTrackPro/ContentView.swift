b458a8c5a109b6304ad7bdd43cbbc097d1de478b
Hoff Silva <hoff.henry@gmail.com>
2025-07-13 15:55:28 -0400

    Initial Commit

---
+++ FitTrackPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitTrackPro
+//
+//  Created by Hoff Henry Pereira da Silva on 2025-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitTrackPro/FitTrackProApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackProApp.swift
+//  FitTrackPro
+//
+//  Created by Hoff Henry Pereira da Silva on 2025-07-13.
+//
+
+import SwiftUI
+
+@main
+struct FitTrackProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
